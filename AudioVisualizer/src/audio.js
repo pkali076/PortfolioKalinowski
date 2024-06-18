c7df1e6d3f44256fbd34499932fbb3ef552a5eda
pkali076 <77549872+pkali076@users.noreply.github.com>
2024-06-18 07:01:57 -0400

    changes in application for Audio Visualizer

--- AudioVisualizer/src/audio.js
+++ AudioVisualizer/src/audio.js
@@ -1,26 +1,23 @@
 let audioCtx;
-
 let element, sourceNode, analyserNode, gainNode;
 
 const DEFAULTS = Object.freeze({
-    gain : .5,
-    numSamples : 256
+    gain: 0.5,
+    numSamples: 256
 });
 
-
-function setupWebaudio(filePath){
+function setupWebaudio(filePath) {
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     audioCtx = new AudioContext();
 
-    element = new Audio();
+    element = document.getElementById('audio'); // Get the existing audio element
 
     loadSoundFile(filePath);
     sourceNode = audioCtx.createMediaElementSource(element);
 
     analyserNode = audioCtx.createAnalyser();
+    analyserNode.fftSize = DEFAULTS.numSamples;
 
-    //fast fourier transform ... 32, 63, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768 default is 2048
-    analyserNode.fftSize = 256;
     gainNode = audioCtx.createGain();
     gainNode.gain.value = DEFAULTS.gain;
 
@@ -28,18 +25,23 @@ function setupWebaudio(filePath){
     analyserNode.connect(gainNode);
     gainNode.connect(audioCtx.destination);
 }
-function loadSoundFile(filePath){
+
+function loadSoundFile(filePath) {
     element.src = filePath;
+    element.load(); // Ensure the audio element is loaded
 }
-function playCurrentSound(){
+
+function playCurrentSound() {
     element.play();
 }
-function pauseCurrentSound(){
+
+function pauseCurrentSound() {
     element.pause();
 }
-function setVolume(value){
+
+function setVolume(value) {
     value = Number(value);
     gainNode.gain.value = value;
 }
 
-export {audioCtx, setupWebaudio, playCurrentSound, pauseCurrentSound, loadSoundFile, setVolume, analyserNode};
+export { audioCtx, setupWebaudio, playCurrentSound, pauseCurrentSound, loadSoundFile, setVolume, analyserNode };
