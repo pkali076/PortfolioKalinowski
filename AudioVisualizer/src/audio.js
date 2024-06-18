ab306994e98c48c5222607969ca9e830271d3360
pkali076 <77549872+pkali076@users.noreply.github.com>
2024-06-18 07:30:52 -0400

    revert back to original coding

--- AudioVisualizer/src/audio.js
+++ AudioVisualizer/src/audio.js
@@ -1,23 +1,27 @@
+
 let audioCtx;
+
 let element, sourceNode, analyserNode, gainNode;
 
 const DEFAULTS = Object.freeze({
-    gain: 0.5,
-    numSamples: 256
+    gain : .5,
+    numSamples : 256
 });
 
-function setupWebaudio(filePath) {
+
+function setupWebaudio(filePath){
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     audioCtx = new AudioContext();
 
-    element = document.getElementById('audio'); // Get the existing audio element
+    element = new Audio();
 
     loadSoundFile(filePath);
     sourceNode = audioCtx.createMediaElementSource(element);
 
     analyserNode = audioCtx.createAnalyser();
-    analyserNode.fftSize = DEFAULTS.numSamples;
 
+    //fast fourier transform ... 32, 63, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768 default is 2048
+    analyserNode.fftSize = 256;
     gainNode = audioCtx.createGain();
     gainNode.gain.value = DEFAULTS.gain;
 
@@ -25,24 +29,18 @@ function setupWebaudio(filePath) {
     analyserNode.connect(gainNode);
     gainNode.connect(audioCtx.destination);
 }
-
-function loadSoundFile(filePath) {
-    console.log(`Loading file: ${filePath}`);
+function loadSoundFile(filePath){
     element.src = filePath;
-    element.load();
 }
-
-function playCurrentSound() {
-    element.play().catch(error => console.log(`Error playing audio: ${error}`));
+function playCurrentSound(){
+    element.play();
 }
-
-function pauseCurrentSound() {
+function pauseCurrentSound(){
     element.pause();
 }
-
-function setVolume(value) {
+function setVolume(value){
     value = Number(value);
     gainNode.gain.value = value;
 }
 
-export { audioCtx, setupWebaudio, playCurrentSound, pauseCurrentSound, loadSoundFile, setVolume, analyserNode };
+export {audioCtx, setupWebaudio, playCurrentSound, pauseCurrentSound, loadSoundFile, setVolume, analyserNode};
--- AudioVisualizer/src/main.js
+++ AudioVisualizer/src/main.js
@@ -1,3 +1,4 @@
+
 import "./navbar.js";
 import "./about.js";
 import "./footer.js";
@@ -14,7 +15,7 @@ const drawParams = {
     showInvert : false,
     redAndBlack : false,
     showSpecial : false
-};
+}
 
 function init(){
     console.log("init called");
@@ -24,16 +25,15 @@ function init(){
     canvas.setupCanvas(canvasElement, audio.analyserNode);
     loop();
 }
-
 function loop(){
     requestAnimationFrame(loop);
     canvas.draw(drawParams);
 
-    let audioData = new Uint8Array(audio.analyserNode.fftSize / 2);
+    let audioData = new Uint8Array(audio.analyserNode.fftSize/2);
     audio.analyserNode.getByteTimeDomainData(audioData);
 }
 
-function setupUI(canvasElement){
+function setupUI(canvasElement){//}, json){
 
     const fsButton = document.querySelector("#fsButton");
     const circlesBox = document.querySelector("#circlesCB");
@@ -43,87 +43,136 @@ function setupUI(canvasElement){
     const invertBox = document.querySelector("#invertCB");
     const redAndBlackBox = document.querySelector("#redAndBlackCB");
     const showSpecialBox = document.querySelector("#showSpecialCB");
+   // const transformSelection = document.querySelector("#transform");
 
     fsButton.onclick = e => {
+        
         utils.goFullscreen(canvasElement);
     };
+    //console.log(json.options[0].showArcs);
 
+   // console.log(arr);
     arcsBox.onclick = e => {
-        drawParams.showArcs = !drawParams.showArcs;
-    };
+      if(drawParams.showArcs == true){
+          drawParams.showArcs = false; 
+      }else{
+          drawParams.showArcs = true;
+         // console.log(drawParams[0].showArcs);
+      }
+  }
+  
+      gradientBox.onclick = e => {
+      if(drawParams.showGradient == true){
+          drawParams.showGradient = false;
+      }else{
+          drawParams.showGradient = true;
+      }
+  }
+      circlesBox.onclick = e => {
+      if(drawParams.showCircles == true){
+          drawParams.showCircles = false;
+      }else{
+          drawParams.showCircles = true;
+      }
+  }
+    clownBox.onclick = e =>{
+      if(drawParams.showClown == true){
+          drawParams.showClown = false;
+      }else{
+          drawParams.showClown = true;
+      }
+  }
+  showSpecialBox.onclick = e =>{
+      if(drawParams.showSpecial == true){
+          drawParams.showSpecial = false;
+      }else{
+          drawParams.showSpecial = true;
+      }
+  }
+  redAndBlackBox.onclick = e=>{
+      if(drawParams.redAndBlack == true){
+          drawParams.redAndBlack = false;
+      }else{
+          drawParams.redAndBlack = true;
+      }
+  }
+  invertBox.onclick = e =>{
+      if(drawParams.showInvert == true){
+          drawParams.showInvert = false;
+      }
+      else{
+          drawParams.showInvert = true;
+      }
+  }
 
-    gradientBox.onclick = e => {
-        drawParams.showGradient = !drawParams.showGradient;
-    };
 
-    circlesBox.onclick = e => {
-        drawParams.showCircles = !drawParams.showCircles;
-    };
 
-    clownBox.onclick = e => {
-        drawParams.showClown = !drawParams.showClown;
-    };
+    playButton.onclick = e =>{
+      console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
+  
 
-    showSpecialBox.onclick = e => {
-        drawParams.showSpecial = !drawParams.showSpecial;
-    };
+  //check if context is in suspended state (autoplay policy)
+  if(audio.audioCtx.state == "suspended"){
+      audio.audioCtx.resume();
+  }
+  console.log(`audioCtx.state ater = ${audio.audioCtx.state}`);
+  if(e.target.dataset.playing == "no"){
+      //if track is crrently paused, play it
+      audio.playCurrentSound();
+      e.target.dataset.playing = "yes"; //our CSS will set the test to "pause"
+      //if track is playing, pause it
+  }else{
+      audio.pauseCurrentSound();
+      e.target.dataset.playing = "no"; // our CSS will set the text to "play"
+  }
 
-    redAndBlackBox.onclick = e => {
-        drawParams.redAndBlack = !drawParams.redAndBlack;
-    };
+};
+    let volumeSlider = document.querySelector("#volumeSlider");
+    let volumeLabel = document.querySelector("#volumeLabel");
 
-    invertBox.onclick = e => {
-        drawParams.showInvert = !drawParams.showInvert;
-    };
-
-    const playButton = document.querySelector("#playButton");
-
-    playButton.onclick = e => {
-        console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
-
-        // Check if context is in suspended state (autoplay policy)
-        if (audio.audioCtx.state === "suspended") {
-            audio.audioCtx.resume();
-        }
-        console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
-        if (e.target.dataset.playing === "no") {
-            // If track is currently paused, play it
-            audio.playCurrentSound();
-            e.target.dataset.playing = "yes"; // Our CSS will set the text to "Pause"
-            e.target.textContent = "Pause";
-        } else {
-            // If track is playing, pause it
-            audio.pauseCurrentSound();
-            e.target.dataset.playing = "no"; // Our CSS will set the text to "Play"
-            e.target.textContent = "Play";
-        }
-    };
-
-    const volumeSlider = document.querySelector("#volumeSlider");
-    const volumeLabel = document.querySelector("#volumeLabel");
-
-    // Add .oninput event to slider
-    volumeSlider.oninput = e => {
-        // Set the gain
+    //add .oninput event to slider
+	volumeSlider.oninput = e => {
+        //set the gain
         audio.setVolume(e.target.value);
-        // Update value of label to match value of slider
-        volumeLabel.innerHTML = Math.round((e.target.value / 2) * 100);
+        //update value of label to match value of slider
+        volumeLabel.innerHTML = Math.round((e.target.value/2 * 100));
     };
-
-    // Set value of label to match initial value of slider
+    
+    //set value of label to match initial value of slider
     volumeSlider.dispatchEvent(new Event("input"));
 
-    // Hookup track <select>
-    const trackSelect = document.querySelector("#trackSelect");
-    // Add .onchange event to <select>
+    //D-- hokup track <Select>
+    let trackSelect = document.querySelector("#trackSelect");
+    //add .onchange event to <select>
     trackSelect.onchange = e => {
         audio.loadSoundFile(e.target.value);
-        // Pause the current track if it is playing
-        if (playButton.dataset.playing === "yes") {
+        //pause the current track if it is playing
+        if(playButton.dataset.playing == "yes"){
             playButton.dispatchEvent(new MouseEvent("click"));
         }
-    };
-}
+    }
 
-// Main
-export { init };
+};
+
+//utils
+const loadJsonFetch = (url, callback) => {
+    fetch(url).then(async response => {
+        //if response successful, return JSON
+        if(response.ok){
+            return response.json();
+        }
+
+        //else throw an error that will be caught below
+        const text = await response.text();
+        throw text;
+    }) //send the response.json() promise to the next .then()
+    .then(json => { //the second promise is resolved and `json` is a JSON object
+        callback(json);
+    }).catch(error => {
+        //error
+        console.log(error);
+    });
+};
+
+//main
+export {init};
